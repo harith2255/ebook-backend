2fdffdb327da5ffd1ddf225632ab9f12223c24d5
Harith <harithpradeepan@gmail.com>
2026-02-26 12:28:40 +0530

    changed

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const allowedOrigins = new Set([
   "http://localhost:3001",
   "http://localhost:4173",
   "https://e-book-gray-one.vercel.app",
-  "e-book-production-6f13.up.railway.app"
+  "https://e-book-production-6f13.up.railway.app"
 ]);
 
 app.use(
